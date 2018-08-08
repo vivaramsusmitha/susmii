9b3a91a3c38ffc83328fbdca56479c7427c3d418
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-08 14:13:18 +0530

    Create 20.java

---
+++ 20.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		   Scanner s = new Scanner(System.in);
+	     int n=s.nextInt();
+        for(int i=1; i <= 5; i++)
+        {
+            System.out.print(+n*i);
+            System.out.print(" ");
+        }
+	}
+}
