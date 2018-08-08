4a142156ac4870959a87ec32e255384a27a4de9c
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-08 19:18:27 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i;
+	//	System.out.println("enter i value");
+		Scanner s=new Scanner(System.in);
+		i=s.nextInt();
+		if(i%2==0)
+		{
+			 System.out.println("Even");
+		}
+		else if(i%2!=0)
+		{
+			System.out.println("Odd");
+			
+		}
+		else
+		{
+			System.out.println("not valid input");
+		}
+
+	}
+}
