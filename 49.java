4098181a57eb1e9467a8aa1ba02add709554871e
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-13 19:05:40 +0530

    Create 49.java

---
+++ 49.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 int x, y, z;
+      //System.out.println("Enter two integers to calculate their sum ");
+      Scanner in = new Scanner(System.in);
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x + y;
+      System.out.println(z);
+
+	}
+}
