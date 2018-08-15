f5da0ff060cf4f4b932675177f6613900243b2f1
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-15 10:43:16 +0530

    Create 55.java

---
+++ 55.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+              scan.close();
+                int product = num1*num2;
+         if(product % 2 == 0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+
+	}
+}
