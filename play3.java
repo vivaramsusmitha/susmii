65b97a3bb46348e728ac4b62ad2b1d4c39afb667
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-20 13:02:54 +0530

    Create play3.java

---
+++ play3.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int num,reversed = 0;
+		Scanner s=new Scanner(System.in);
+		num=s.nextInt();
+
+        while(num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println(+ reversed);
+
+	}
+}
