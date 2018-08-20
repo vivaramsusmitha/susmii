acf6cf0cead7f3321891ddfbb64da0012f03cbec
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-20 11:22:47 +0530

    Create pr52.java

---
+++ pr52.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int n3=sc.nextInt();
+		int n4=sc.nextInt();
+		int n5=sc.nextInt();
+		int n6=sc.nextInt();
+		int n7=sc.nextInt();
+		int n8=sc.nextInt();
+		if(n1==n2&&n5==n6&&n3==n8&&n4==n7)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+
+
+
+
+
+
+
+	}
+}
