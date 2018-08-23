76abe61ff4f181a90f1a2938c66281bba1f8e67a
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-23 12:42:23 +0530

    Create 72.java

---
+++ 72.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.next();
+		int count=0;
+		for(int i=0;i<=str.length()-1;i++)
+		{
+			if(str.charAt(i)=='a'||str.charAt(i)=='A'||str.charAt(i)=='e'||str.charAt(i)=='E'||str.charAt(i)=='i'||str.charAt(i)=='I'||str.charAt(i)=='o'||str.charAt(i)=='O'||str.charAt(i)=='u'||str.charAt(i)=='U')
+			{
+				count++;
+			}
+		}
+		if(count==0)
+		{
+			System.out.print("no");
+		}
+		else
+		{
+			System.out.print("yes");
+		}
+	}
+}
