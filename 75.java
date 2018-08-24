fe2942506d16ee3b19f8dfba15b7355a74a141bf
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-24 13:00:26 +0530

    Create 75.java

---
+++ 75.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.next();
+		StringBuilder sb=new StringBuilder(str);
+		int n=str.length();
+		if(n%2!=0)
+		{
+			sb.setCharAt(n/2,'*');
+			str=sb.toString();
+			System.out.print(str);
+		}
+		else
+		{
+			sb.setCharAt(n/2,'*');
+			sb.setCharAt(n/2-1,'*');
+			str=sb.toString();
+		}
+	}
+}
