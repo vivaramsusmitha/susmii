ddbfa459d9f457a97cda95dcf32a3158bf0a643c
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-24 11:55:37 +0530

    Create 79.java

---
+++ 79.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1,n2,temp,sq=1;
+		Scanner s=new Scanner(System.in);
+		n1=s.nextInt();
+		n2=s.nextInt();
+		int res=n1*n2;
+		temp=res/2;
+		for(int i=temp;i>=0;i--)
+		{
+			sq=i*i;
+			if(sq==res)
+			{
+				break;
+			}
+		}
+		if(sq==res)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+		
+		
+	}
+}
