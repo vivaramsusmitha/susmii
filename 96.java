34685c6c7c0e8348bea06855c537acd3d337a665
vivaramsusmitha <41729707+vivaramsusmitha@users.noreply.github.com>
2018-08-30 12:11:25 +0530

    Create 96.java

---
+++ 96.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt(),d=s.nextInt(),n=s.nextInt();
+		System.out.print(+n*(2*a+(n-1)*d)/2);
+	}
+}
